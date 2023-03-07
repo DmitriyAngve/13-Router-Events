1ed2778a422751fdbb80de2533c5cdf6341ada65
Dmitriy Angve <kirirch129@gmail.com>
2023-03-07 13:53:36 +0300

    #

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,29 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import ErrorPage from './pages/Error';
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
-} from './pages/EventDetail';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
-import { action as manipulateEventAction } from './components/EventForm';
-import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+} from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import AuthenticationPage from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events',
+        path: "events",
         element: <EventsRootLayout />,
         children: [
           {
@@ -31,8 +32,8 @@ const router = createBrowserRouter([
             loader: eventsLoader,
           },
           {
-            path: ':eventId',
-            id: 'event-detail',
+            path: ":eventId",
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
@@ -41,21 +42,22 @@ const router = createBrowserRouter([
                 action: deleteEventAction,
               },
               {
-                path: 'edit',
+                path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
               },
             ],
           },
           {
-            path: 'new',
+            path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
           },
         ],
       },
+      { path: "auth", element: <AuthenticationPage /> },
       {
-        path: 'newsletter',
+        path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
       },
@@ -68,3 +70,9 @@ function App() {
 }
 
 export default App;
+
+// 312. Project Setup & Route Setup
+// STEP 1:
+// 1.1 Here I wanna add a new route definition for "auth" route. The "auth" should still be part of my route layout "<RootLayout>". I still want to have that navigation on top of it and so on. It will be a sibling route to "<HomePage>" and entire route stack of my events routes. (path: "auth", element: <AuthenticationPage />)
+// GO TO MainNavigation.js for adding Auth to navigation --->>>
+// 312. Project Setup & Route Setup
--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,19 +1,22 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+// import { useState } from "react";
+import { Form, Link, useSearchParams } from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  // const [seacrhParams, setSearchParams] = useSearchParams();
+  const [seacrhParams] = useSearchParams();
+  const isLogin = seacrhParams.get("mode") === "login";
+  // const [isLogin, setIsLogin] = useState(true);
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  // function switchAuthHandler() {
+  //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  // }
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -23,9 +26,10 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
-            {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          {/* <Link to="?mode=signup">{isLogin ? "Create new user" : "Login"}</Link> */}
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Create new user" : "Login"}
+          </Link>
           <button>Save</button>
         </div>
       </Form>
@@ -34,3 +38,20 @@ function AuthForm() {
 }
 
 export default AuthForm;
+
+// 313. Working with Query Parameters
+//
+// STEP 1:
+// 1.1 Get rid switch off handler function and "useState" call. And instead now I want to replace my button sown there, which i use for toggling with a link.
+// 1.2 import "Link" component from r-r-d. And then replace "button" with that "link".
+// 1.3 Set "to=" "" prop on that link.
+// 1.4 Let's populate prop on link. /// "<Link to="?mode=signup">" "?" - query parameter, and then the name of parameter. And then should be either log in or sign up, depending on whether we are currently on the login or signup page. That must be set dynamically.
+// For setting dynamic query parameters we import special hook from r-r-d
+// 1.5 Import "useSearchParams" from r-r-d.
+// 1.6 Call "useSearchParams()" and this actually returns an array. We can use array destructuring to get access to the elements in that array. There are two elements in taht array.
+// 1.7 First elemet is an object that gives us access to the currently set query parameters ("seacrhParams"); and the second value we get from that array is a function that allows us to update the currently set query parameters ("setSearchParams").
+// I don't need that function here, because I will update the query parameter with help of that link that sets the query parameter, but I do need to get access to the currently active query parameters.
+// 1.8 Therefore. i will just get that first element and then here we can get the information whether we are in login mode or not by accessing this search patterns object. And then there is a ".get("")" method, which allows us to retrive the value for a specific query parameter. Here it's the "mode" query parameter I wanna retrieve the value for, and I wanna check if the value is equal to "login" /// "const isLogin = seacrhParams.get("mode") === "login";" (if "mode" set to "login").
+// If it set to any other value, then I assume that I'm in signup mode.
+// 1.9 Use this "isLogic" in our "Link" /// "<Link to={`?mode=${isLogin ? "signup" : "login"}`}>"
+// 313. Working with Query Parameters
